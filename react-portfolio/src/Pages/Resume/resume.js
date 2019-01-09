52025f085f015a087a665159f27870a5a400aec9
Allison Driver <allisondriver@allisons-mbp.attlocal.net>
2019-01-09 11:44:33 -0600

    updated resumé

--- react-portfolio/src/Pages/Resume/resume.js
+++ react-portfolio/src/Pages/Resume/resume.js
@@ -10,7 +10,7 @@ const Resume = () => {
 
     return (
       <div className = "App">
-       <a  className="resumeLink" href = "https://docs.google.com/document/d/1DxVyld6atROCaqW_g-RVUSUle9VM9jhNI9z8FrbFTWQ/edit?usp=sharing">Resumé</a>
+       <a  className="resumeLink" href = "https://docs.google.com/document/d/1VEntXTIjb3kF8sIPlKkvDS9V2tZJS1RgKkAhthd_lno/edit?usp=sharing">Resumé</a>
       </div>
     );
   }
