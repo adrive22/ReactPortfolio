76cf0f5666f454c0655c776d996eef0ba3ead5df
Allison Driver <allisondriver04@gmail.com>
2024-03-28 13:13:45 -0500

    adding resume link

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -45,7 +45,7 @@ function Header () {
         <MenuItem
         className="wordNav">
         <Link
-         to="https://docs.google.com/document/d/1VEntXTIjb3kF8sIPlKkvDS9V2tZJS1RgKkAhthd_lno/edit"
+         to="https://drive.google.com/file/d/1QlC_Dwnz53Eg6LJI-GxHKO6DoMoBp8-3/view?usp=sharing"
         >
         Résumé
         </Link>
@@ -73,7 +73,7 @@ function Header () {
               </DropdownItem>
              <DropdownItem
              className="dropdownLink"
-            href="https://docs.google.com/document/d/1VEntXTIjb3kF8sIPlKkvDS9V2tZJS1RgKkAhthd_lno/edit">
+            href="https://drive.google.com/file/d/1QlC_Dwnz53Eg6LJI-GxHKO6DoMoBp8-3/view?usp=sharing">
               Résumé
               </DropdownItem>
               <DropdownItem
